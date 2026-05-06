d65f907a999cabc3bfd9dbb01974db85ed1c9123
Vincent Roberson <ghub@varmail.org>
2026-05-06 18:31:35 +0000

    redid the benchmark program to conform to this project

--- benchmark.sh
+++ benchmark.sh
@@ -1,38 +1,36 @@
 #!/bin/bash
 
 LANG_ARG="$1"
-DIR_ARG="$2"
-FULL_DIR="./test-output/${DIR_ARG}"
-mkdir -p ${FULL_DIR}
-mkdir -p ${FULL_DIR}/rust_lang
-mkdir -p ${FULL_DIR}/C_lang
+MACH_DIR_ARG="$2" # for different hardware
+RUN_COUNT_ARG="${3:-5}"
+FULL_DIR="./test-output/${MACH_DIR_ARG}"
+mkdir -p "${FULL_DIR}"
+mkdir -p "${FULL_DIR}/rust_lang"
+mkdir -p "${FULL_DIR}/C_lang"
 
-N_VALUES=("1000000000" "10000000000" "100000000000")
-TEST_NUMBERS=("1" "2" "3")
+THREAD_COUNT_VALUES=("1" "4" "8")
 
-for i in "${!N_VALUES[@]}"; do
-    N="${N_VALUES[$i]}"
-    TEST_NUMBER="${TEST_NUMBERS[$i]}"
-
+for N in "${THREAD_COUNT_VALUES[@]}"; do
     if [ "$LANG_ARG" = "rust" ]; then
-        N_VALUE=$N cargo build --release
-        CMD="./target/release/final-project-main"
-        OUTPUT="${FULL_DIR}/rust_lang/rust_benchmark_results${TEST_NUMBER}.txt"
+        THREAD_COUNT=$N cargo run --release -p rust-fft
+        OUTPUT="${FULL_DIR}/rust_lang/rust_benchmark_results_t${N}.txt"
         > "$OUTPUT"
     elif [ "$LANG_ARG" = "C" ]; then
+        cd c-fft/
         make clean
-        make N_VALUE=$N
-        CMD="./trap"
-        OUTPUT="${FULL_DIR}/C_lang/C_benchmark_results${TEST_NUMBER}.txt"
+        make THREAD_COUNT=$N
+        CMD="./fft_t$N"
+        cd ..
+        OUTPUT="${FULL_DIR}/C_lang/C_benchmark_results_t${N}.txt"
         > "$OUTPUT"
     fi
 
-    echo "n = $N" >> "$OUTPUT"
+    echo "thread_count = $N" >> "$OUTPUT"
     echo "" >> "$OUTPUT"
 
-    for j in $(seq 1 5); do
+    for j in $(seq 1 "$RUN_COUNT_ARG"); do
         echo "Run $j:" >> "$OUTPUT"
-        { time $CMD; } >> "$OUTPUT" 2>&1
+        { time "$CMD"; } >> "$OUTPUT" 2>&1
         echo "" >> "$OUTPUT"
     done
 done
